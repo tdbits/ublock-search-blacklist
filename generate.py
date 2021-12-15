4ab5f31d48fbcae0b7d83500b9874436e57a13fa
Thomas David <contact@code-geek.de>
2021-12-15 22:00:31 +0100

    Adds header to blacklist files

--- generate.py
+++ generate.py
@@ -4,12 +4,23 @@
 #
 
 from typing import List, AnyStr
+from datetime import datetime
 
 
 def main():
     urls = load_urls()
     generate_for_google(urls)
     generate_for_startpage(urls)
+
+
+def header() -> List[AnyStr]:
+    return [
+        '! Title: Search Blacklist',
+        f'! Updated: {datetime.now().strftime("%a, %d %b %Y %H:%M:%S %z")}',
+        '! Expires: 1 day (update frequency)',
+        '! Homepage: https://github.com/tdbits/ublock-search-blacklist',
+        '! License: https://github.com/tdbits/ublock-search-blacklist/blob/main/LICENSE'
+    ]
 
 
 def load_urls() -> List[AnyStr]:
@@ -23,14 +34,18 @@ def generate_for_google(urls: List[AnyStr]) -> List[AnyStr]:
     blacklist = map(lambda url: f'google.*##.g:has(a[href*="{url}"])', urls)
 
     with open('google.txt', 'w') as blacklist_file:
-        blacklist_file.writelines('\n'.join(blacklist))
+        blacklist_file.write('\n'.join(header()))
+        blacklist_file.write('\n')
+        blacklist_file.write('\n'.join(blacklist))
 
 
 def generate_for_startpage(urls: List[AnyStr]) -> List[AnyStr]:
     blacklist = map(lambda url: f'startpage.*##.w-gl__result:has(a[href*="{url}"])', urls)
 
     with open('startpage.txt', 'w') as blacklist_file:
-        blacklist_file.writelines('\n'.join(blacklist))
+        blacklist_file.write('\n'.join(header()))
+        blacklist_file.write('\n')
+        blacklist_file.write('\n'.join(blacklist))
 
 
 if __name__ == '__main__':
